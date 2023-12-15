7e12dbb315a82aac48673e1464219f4b509992a1
helenakristina <hklucia@gmail.com>
2023-12-15 16:53:35 -0600

    Added docstrings and formatted code with Ruff

--- app.py
+++ app.py
@@ -10,17 +10,24 @@ import pandas as pd
 from result import Result
 
 
-logging.basicConfig(format='%(levelname)s:%(message)s',
-                    level=logging.INFO)
-
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 NUM_THREADS = 8
 
 
 def parse_file(path: str) -> pd.DataFrame:
+    """Function to parse a single file with graceful failures.
+    Will return None if file does not conform to desired format.
 
+    Args:
+        path (str): File to parse. Accepts http(s), local file system,
+        hdfs, s3, or gcs file paths
+
+    Returns:
+        pd.DataFrame: Dataframe if file can be parsed, None if not
+    """
     try:
         df = pd.read_csv(path, skipinitialspace=True)
-    except Exception as err:
+    except Exception:
         logging.info(f"File failed: {path}")
         return None
     if "fname" not in df.columns:
@@ -29,13 +36,15 @@ def parse_file(path: str) -> pd.DataFrame:
     return df
 
 
-if __name__ == "__main__":
+def main(files: list) -> None:
+    """_summary_
 
-    files = sys.argv[1:]
+    Args:
+        files (list): List of file paths to parse
+    """
     if not files:
         logging.info("No files to parse.")
         exit(0)
-
     result = Result()
     result.num_threads = NUM_THREADS
     client = dask.distributed.Client(nthreads=NUM_THREADS)
@@ -57,10 +66,10 @@ if __name__ == "__main__":
         result.median_age = median
         average = ddf["age"].mean().compute()
         result.average_age = average
-        median_record = ddf.query(f'age == {median}').compute().iloc[0]
+        median_record = ddf.query(f"age == {median}").compute().iloc[0]
         if not median_record.empty:
-            result.median_record_fname = median_record['fname']
-            result.median_record_lname = median_record['lname']
+            result.median_record_fname = median_record["fname"]
+            result.median_record_lname = median_record["lname"]
 
         stop = timeit.default_timer()
         result.elapsed_sec = stop - start
@@ -70,3 +79,7 @@ if __name__ == "__main__":
     except Exception as err:
         logging.error(err)
         exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- result.py
+++ result.py
@@ -2,21 +2,32 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Result():
+class Result:
+    """_summary_
+
+    Returns:
+        Result: Data Structure to persist result data calculated in main function
+    """
+
     average_age: int = 0
     elapsed_sec: int = 0
     median_age: int = 0
-    median_record_fname: str = ''
-    median_record_lname: str = ''
+    median_record_fname: str = ""
+    median_record_lname: str = ""
     num_threads: int = 0
 
-    def get_median_record_str(self):
+    def get_median_record_str(self) -> str:
+        """_summary_
+
+        Returns:
+            str: Description of median record, if found.
+        """
         if self.median_record_fname:
             return f"A median record is {self.median_record_fname} {self.median_record_lname}."
         else:
             return "No median record found."
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"""
             This program completed in {self.elapsed_sec} seconds
             using {self.num_threads} threads.
